a5945fe5ce4abc687595a35b0b14b9f17b4dfbc5
Krishna <krishnasmvec97@gmail.com>
2025-08-07 23:30:04 +0530

    LED BLINKING

--- LED_BLINKING/src/main.cpp
+++ LED_BLINKING/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
-
+#define LED 2
 // put function declarations here:
-int myFunction(int, int);
+// int myFunction(int, int);
+
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // int result = myFunction(2, 3);
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED is on");
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED is off");
+  delay(1000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
